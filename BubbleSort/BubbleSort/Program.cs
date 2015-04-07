5c0e5acb66a3cefc649eeba455d9e0f605b63dfa
Danny <catalystcoding@gmail.com>
2015-04-06 22:11:10 -0500

    Initial commit

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberInputs = string.Empty;
+            var sortObject = new BubbleSort();
+            
+            Console.WriteLine("Enter a comma separated list of numbers that will be sorted:");
+            numberInputs = Console.ReadLine();
+            sortObject.numbers = numberInputs.Split(',').Select(double.Parse).ToList();
+            
+            Console.WriteLine("Your input was {0}", numberInputs);
+            sortObject.Sort();
+            Console.WriteLine("Your sorted items are:");
+            sortObject.DisplayList();
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Class that will contain a list of input numbers and the methods to handle the sorting
+    /// </summary>
+    public class BubbleSort
+    {
+        public List<double> numbers { get; set; } 
+
+        public BubbleSort()
+        {
+            // ensure an empty list is created
+            numbers = new List<double>();
+        }
+
+        /// <summary>
+        /// Handles the swap of two number
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void Swap(ref double x, ref double y)
+        {
+            var temp = x;
+            x = y;
+            y = temp;
+        }
+
+        /// <summary>
+        /// The actual bubble sort method
+        /// </summary>
+        public void Sort()
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = i+1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        // sets temp values to pass by reference
+                        var num1 = numbers[i];
+                        var num2 = numbers[j];
+                        Swap(ref num1,ref num2);
+                        // re-assign the numbers
+                        numbers[i] = num1;
+                        numbers[j] = num2;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Parses the numbers property to determine if the numbers are in ascending order
+        /// </summary>
+        /// <param name="index">Optional integer, used typically only for the recursive calls</param>
+        /// <returns></returns>
+        public Boolean IsSorted(int index = 0)
+        {
+            if (index== numbers.Count - 1)
+            {
+                return true;
+            } else if (numbers[index] < numbers[index+1])
+            {
+                return IsSorted(index + 1);
+            } else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Simple method for displaying the list of numbers
+        /// </summary>
+        public void DisplayList()
+        {
+            foreach (var num in numbers)
+            {
+                Console.Write(num.ToString() + ' ',0,num.ToString().Length);
+            }
+        }
+    }
+}
---
+++ BubbleSort/BubbleSortTests/UnitTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using Xunit;
+
+namespace BubbleSortTests
+{
+    public class UnitTests
+    {
+        [Description("Ensures swap of two integers properly occurs")]
+        [Fact]
+        public void TestSwapMethod() {
+            var number1 = 15d;
+            var number2 = 10d;
+            // ensure variables were properly assigned
+            Assert.Equal(15d, number1);
+            Assert.Equal(10d, number2);
+            // Process swap
+            var client = new BubbleSort.BubbleSort();
+            client.Swap(ref number1,ref number2);
+            // ensure variables do not equal their original value
+            Assert.NotEqual(15d, number1);
+            Assert.NotEqual(10d, number2);
+            // ensure variables were properly swapped: eg. 15 in number2, 10 in number1
+            Assert.Equal(10d, number1);
+            Assert.Equal(15d, number2);
+        }
+
+        [Description("Processes a simple sort and ensures correctness")]
+        [Fact]
+        public void TestSortMethod() {
+            // new object
+            var client = new BubbleSort.BubbleSort();
+            // add unsorted numbers
+            client.numbers.Add(6.00);
+            client.numbers.Add(5.13);
+            client.numbers.Add(5.131);
+            // attempt to sort
+            client.Sort();
+            // ensure numbers are sorted
+            Assert.Equal(client.numbers[0],5.13);
+            Assert.Equal(client.numbers[1],5.131);
+            Assert.Equal(client.numbers[2], 6.00);
+        }
+
+        [Description("Ensures IsSortedMethod is working as intended")]
+        [Fact]
+        public void TestIsSortedMethod()
+        {
+            // create two objects
+            var isNotSorted = new BubbleSort.BubbleSort();
+            var isSorted = new BubbleSort.BubbleSort();
+            // add numbers to each object
+            isNotSorted.numbers.Add(1.00);
+            isNotSorted.numbers.Add(0.90);
+            isNotSorted.numbers.Add(1.20);
+            //
+            isSorted.numbers.Add(1.00);
+            isSorted.numbers.Add(1.20);
+            isSorted.numbers.Add(9.00);
+            // check method
+            Assert.Equal(isNotSorted.IsSorted(), false);
+            Assert.Equal(isSorted.IsSorted(),true);
+        }
+    }
+}
